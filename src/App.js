252b202a45c089c0dc0acaa58c2576b77ed23151
unknown <dinhquangngu09@example.com>
2021-09-01 10:16:57 +0700

    clear architecture

--- src/App.js
+++ src/App.js
@@ -1,102 +1,15 @@
+import React from 'react';
 import './App.scss';
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="trello-master">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fhan2-3.fna.fbcdn.net/v/t1.6435-9/115931985_992575664500121_4036679232503007267_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_ohc=XzL999VfT5cAX-lTtGd&_nc_ht=scontent.fhan2-3.fna&oh=dba417ae2eda12ca7db736f3b8e1ff93&oe=615342F7" alt="" />
-              Title: Quang Ngữ Developer
-            </li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-            <li>add what you'd like to work on the below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AppBar.scss';
+
+function AppBar() {
+    return (
+        // <div>
+            <nav className="navbar-app">App Bar</nav>
+        // </div>   
+    );
+}
+
+export default AppBar;
